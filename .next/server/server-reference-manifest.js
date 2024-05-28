508d31a57c6ec3da905ae11be7d005387be8b0dc
emenella <erwan.menella@hotmail.fr>
2024-05-28 14:34:20 +0200

    SST portfolio in master

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ekzbnaIwGk58ze6o+Ooad1AV5V75nYzCYlN5HIuOCAY=\"\n}"
---
+++ .next/static/chunks/_app-pages-browser_src_locales_fr_common_json.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["_app-pages-browser_src_locales_fr_common_json"],{
+
+/***/ "(app-pages-browser)/./src/locales/fr/common.json":
+/*!************************************!*\
+  !*** ./src/locales/fr/common.json ***!
+  \************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"title":"Portfolio Emenella","description":"Découvrez le portfolio d\'emenella, un développeus web fullstack passionnée, spécialisée dans la création d\'applications web modernes. Explorez mes projets, mes compétences et mon expérience professionnelle mis en œuvre avec Next.js, React, Node.js et Fastify."},"homepage":{"description":"Un développeur innovant originaire d\'Arles, inspiré par Van Gogh, qui façonne une nouvelle vision de l\'avenir numérique.","skills":[{"title":"Node.js","description":"Node.js est un moteur d\'exécution JavaScript basé sur le moteur JavaScript V8 de Chrome."},{"title":"Bun","description":"Bun est un runtime JavaScript, un gestionnaire de packages et un bundler de lanceurs."},{"title":"Nest.js","description":"Un framework Node.js progressif pour construire des applications côté serveur efficaces, fiables et évolutives."},{"title":"Fastify","description":"Fastify est un framework web dont l\'objectif est d\'offrir aux développeurs la meilleure expérience possible avec un minimum de frais généraux et une puissante architecture de plugins."},{"title":"Express","description":"Express.js est un framework pour construire des applications web basées sur Node.js."},{"title":"React","description":"Une bibliothèque JavaScript pour la construction d\'interfaces utilisateur."},{"title":"Next.js","description":"Next.js est un framework gratuit et open source s\'appuyant sur la bibliothèque JavaScript React et sur la technologie Node.js."},{"title":"Tailwind CSS","description":"Tailwind CSS est un framework CSS open source."},{"title":"Framer Motion","description":"Une bibliothèque d\'animation JavaScript simple et puissante."},{"title":"AWS","description":"Amazon Web Services offre des services d\'informatique en cloud fiables, évolutifs et peu coûteux."}],"projects":[{"title":"Portfolio","description":"Mon site portfolio, réalisé avec Next.js et Shadcn/Tailwind CSS."},{"title":"ft_transcendence","description":"Un site web pour un simple jeu de pong multijoueur en ligne."},{"title":"ft_irc","description":"Un simple serveur et client IRC."}]}}');
+
+/***/ })
+
+}]);
